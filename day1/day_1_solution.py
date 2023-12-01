f2b09fbbc58f1fe951c650e23e5497d503db07ad
zorua162 <55893520+Zorua162@users.noreply.github.com>
2023-12-01 11:34:37 +0000

    Finished day1 part1 working on day1 part2

--- day1/day_1_solution.py
+++ day1/day_1_solution.py
@@ -1,11 +1,57 @@
+import re
 
-def main(data_path):
+regex = r"[a-z]"
+number_words_dict = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+    "zero": "0",
+}
+
+
+def part1(data_path: str):
     with open(data_path, "r") as f_obj:
         data = f_obj.read().split("\n")
-    print(data)
-    return data
+    result = [re.sub(regex, "", line, 0) for line in data]
+    print(result)
+    numbers = [line[0] + line[-1] for line in result if line != ""]
+    output = sum([int(number) for number in numbers])
+    return output
+
+
+def replace_words(data: list) -> list:
+    out = []
+    for line in data:
+        current_line = line
+        for word, value in number_words_dict.items():
+            print(word, value)
+            current_line = current_line.replace(word, value)
+        out.append(current_line)
+    return out
+
+
+def part2(data_path: str):
+    with open(data_path, "r") as f_obj:
+        data = f_obj.read().split("\n")
+    # replace all words that are numbers with their number
+    data = replace_words(data)
+    print(f"words replaced data {data}")
+
+    result = [re.sub(regex, "", line, 0) for line in data]
+    print(f"regex result {result}")
+    numbers = [line[0] + line[-1] for line in result if line != ""]
+    output = sum([int(number) for number in numbers])
+    return output
 
 
 if __name__ == "__main__":
-    main("data.txt")
-    main("example_data.txt")
+    # print(part1("example_data.txt"))
+    # print(part1("data.txt"))
+    print(part2("example_data_part2.txt"))
+    # print(part2("data.txt"))
--- day1/test_day_1.py
+++ day1/test_day_1.py
@@ -1,13 +1,30 @@
-from day1.day_1_solution import main
+from day1.day_1_solution import part1, part2, replace_words
 import pytest
 
 
-@pytest.mark.skip("Answer is from AOC website")
-def test_data_output():
-    output = main("data.txt")
-    assert output == "currently unknown"
+def test_example_data_output_part1():
+    output = part1("example_data.txt")
+    assert output == 142
 
 
-def test_example_data_output():
-    output = main("example_data.txt")
-    assert output == ["1", "2", "3", ""]
+def test_data_output_part1():
+    output = part1("data.txt")
+    assert output == 54390
+
+
+def test_replace_words():
+    word_list = ["1one2two", "three3four4"]
+    out = replace_words(word_list)
+    print(out)
+    assert ["1122", "3344"] == out
+
+
+def test_example_data_output_part2():
+    output = part2("example_data_part2.txt")
+    assert output == 281
+
+
+@pytest.mark.skip("Solution is from AOC website")
+def test_data_output_part2():
+    output = part2("data.txt")
+    assert output == "Currently Unknown"
